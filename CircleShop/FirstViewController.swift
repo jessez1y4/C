280302322fb24515a681b809b3c7df2c4371a1e6
yue zheng <zhengyuee@gmail.com>
2014-08-31 21:00:14 -0700

    Initial Commit

---
+++ CircleShop/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CircleShop
+//
+//  Created by yue zheng on 8/31/14.
+//  Copyright (c) 2014 yue zheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
