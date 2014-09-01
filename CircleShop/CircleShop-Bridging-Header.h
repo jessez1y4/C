deedbd4bfb8fc5d38d89ffc22464d10f8647c6bc
yue zheng <zhengyuee@gmail.com>
2014-09-01 14:12:48 -0700

    add pod file

---
+++ CircleShop/CircleShop-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  CircleShop-Bridging-Header.h
+//  CircleShop
+//
+//  Created by yue zheng on 9/1/14.
+//  Copyright (c) 2014 yue zheng. All rights reserved.
+//
+
+#ifndef CircleShop_CircleShop_Bridging_Header_h
+#define CircleShop_CircleShop_Bridging_Header_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <REMenu.h>
+
+
+#endif
--- CircleShop/CircleViewController.swift
+++ CircleShop/CircleViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class CircleViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
